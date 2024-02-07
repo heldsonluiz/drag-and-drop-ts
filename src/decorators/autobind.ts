14e8cd96df9a76fe21b2d2d58b9c68909c2a4932
Heldson Luiz Silva <heldson@kanttum.com>
2024-02-07 17:44:50 -0300

    ♻️ Organize files and folder
    Organize files and folders using namespace

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,14 @@
+namespace App {
+  /** AUTOBIND DECORATOR */
+  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value
+    const adjustedDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this)
+        return boundFn
+      }
+    }
+    return adjustedDescriptor
+  }
+}
