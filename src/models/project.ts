eb395b4e6bd756ac8b7c460791e1099407187d43
Heldson Luiz Silva <heldson@kanttum.com>
2024-02-07 18:07:13 -0300

    ♻️ Using ES Modules

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,14 +1,12 @@
-namespace App {
-  /** PROJECT TYPE */
-  export enum ProjectStatus { ACTIVE, FINISHED }
+/** PROJECT TYPE */
+export enum ProjectStatus { ACTIVE, FINISHED }
 
-  export class Project {
-    constructor(
-      public id: string,
-      public title: string,
-      public description: string,
-      public people: number,
-      public status: ProjectStatus
-    ) { }
-  }
+export class Project {
+  constructor(
+    public id: string,
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus
+  ) { }
 }
