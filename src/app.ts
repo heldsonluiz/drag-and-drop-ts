7124c5bd08d692366ed1ba42336ba18fb3f316c6
Heldson Luiz Silva <heldson@kanttum.com>
2024-02-07 17:50:35 -0300

    ♻️ Refactor namespaced files

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,7 @@
-/// <reference path="models/drag-drop.ts"/>
-/// <reference path="models/project.ts"/>
-/// <reference path="state/project-state.ts"/>
-/// <reference path="util/validation.ts"/>
-/// <reference path="decorators/autobind.ts"/>
 /// <reference path="components/project-input.ts"/>
 /// <reference path="components/project-list.ts"/>
 
 namespace App {
-
-  /** COMPONENT BASE CLASS */
-
-
-  /** PROJECT ITEM CLASS */
-
-
-  /** PROJECT LIST CLASS */
-
-
-  /** PROJECT INPUT CLASS */
-
-
-
   new ProjectInput()
   new ProjectList('active')
   new ProjectList('finished')
